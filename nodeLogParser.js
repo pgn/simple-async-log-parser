0614d588fb608f85a1be5df957f634672f95807f
maciej <maciej@waw-DL010.(none)>
2013-09-23 17:41:50 +0200

    logs and new parser

--- nodeLogParser.js
+++ nodeLogParser.js
@@ -8,41 +8,63 @@ var fs = require('fs'),
     readline = require('readline');
 
 
-var pathToLogs = './logs/';
+var pathToLogs = './source/';
 var pathToParsedLogs = './parsed/';
 
 var expresionsToClear = [
-    new RegExp('Sep (0-9){2}', 'g'),
-    'cd-adb-prod apache2: /var/www/adb-tnr2850st-v2.stb.go.canaldigital.com',
+    new RegExp('Sep [0-9]{2} ', 'g'),
+    new RegExp('cd-adb-prod apache2: /var/www/adb-tnr2850st-v2.stb.go.canaldigital.com [a-z0-9]{8,10} '),
     new RegExp(': ', 'g'),
     new RegExp('  ', 'g')
 ];
 
 var pareseLogFile = function(filename){
     if (0 !== filename.indexOf('log')) {
-        
+
+        console.log ('File ' + filename + ' skipped');
         return;
     }
-    
-    var instream = fs.createReadStream(pathToLogs + filename);
-    var outstream = fs.createWriteStream(pathToParsedLogs + filename);
-    var rl = readline.createInterface(instream, outstream);
-    
-    console.log('START with ' + filename);
-    
-    rl.on('line', function(line) {
-        expresionsToClear.map(function(expr) {
-            line = line.replace(expr, '');
-        })
-        outstream.write(line + "\n");
+
+    fs.exists(pathToParsedLogs + filename , function (exists){
+        if (exists) {
+            console.log ('File ' + filename + ' already parsed');
+        } else {
+            console.log('START with ' + filename);
+
+            var instream = fs.createReadStream(pathToLogs + filename);
+            var outstream = fs.createWriteStream(pathToParsedLogs + filename);
+            var rl = readline.createInterface(instream, outstream);
+
+            var startFlag = false;
+
+            rl.on('line', function(line) {
+                if (!startFlag && -1 !== line.indexOf('--== START! ==--')) {
+                    startFlag = true;
+                }
+                if (!startFlag || -1 !== line.indexOf('language::und')) {
+                    //Skip the lines before start or from player debug
+                    return
+                }
+                expresionsToClear.map(function(expr) {
+                    line = line.replace(expr, ' ');
+                })
+                outstream.write(line + "\n");
+            });
+
+            rl.on('close', function() {
+                console.log('DONE with ' + filename);
+                startFlag = false;
+            });
+        }
     });
-    
-    rl.on('close', function() {
-        console.log('DONE with ' + filename);
-    });
-    
 }
 
 fs.readdir(pathToLogs, function(err, files) {
-    files.map(pareseLogFile);
+
+    if (err) throw err;
+    if (files) {
+        files.map(pareseLogFile);
+    } else {
+        console.log('No log files found');
+    }
 });
